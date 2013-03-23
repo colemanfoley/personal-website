0894f8d8a57075d7ffafa191d50c66f289b08a9c
Coleman Foley <colemanfoley@gmail.com>
2013-03-23 14:35:35 -0700

    Initial commit of Bootstrap branch.

--- apps/ProgrammingPath/functions.js
+++ apps/ProgrammingPath/functions.js
@@ -10,15 +10,15 @@ function backendFunction(){
 	rubyResponse = "Sweet! Ruby is one of the quickest ways to get an app up and running.";
 	phpResponse = "You can't go wrong with something as ubiquitous as PHP.";
 	//This function gives some feedback on the user's choice of backend language.
-	if (backendSelection == "Python"){
+	if (backendSelection === "Python"){
 		document.getElementById('backendDummy').innerHTML=pythonResponse;
 	}
 	
-	else if (backendSelection == "Ruby"){
+	else if (backendSelection === "Ruby"){
 		document.getElementById('backendDummy').innerHTML=rubyResponse;
 	}
 		
-	else if (backendSelection == "PHP"){
+	else if (backendSelection === "PHP"){
 		document.getElementById('backendDummy').innerHTML=phpResponse;
 	}
 };
@@ -32,12 +32,12 @@ function learningStyleFunction(){
 	//These variables store the responses to the user's learning style choices.
 	independentResponse = "You'll have no shortage of options for learning independently.";
 	structuredResponse = "There's no shame in admitting you need some structure";
-	if (learningStyleSelection == "Independent")
+	if (learningStyleSelection === "Independent")
 		{
 		document.getElementById('learningStyleDummy').innerHTML=independentResponse;
 		}
 	
-	else if (learningStyleSelection == "Structured")
+	else if (learningStyleSelection === "Structured")
 		{
 		document.getElementById('learningStyleDummy').innerHTML=structuredResponse;
 		}
@@ -48,29 +48,28 @@ function conclusionGenerator(){
 	learningStyleSelection = learningStyleElement.options[learningStyleElement.selectedIndex].value;
 
 	structuredRuby = "Dev Bootcamp is a great way to go. It is a very structured, focused 10-week course that can take you from zero to world-class Ruby beginner in 10 weeks.";
-	structuredPython= "Zed Shaw's 'Learn Python the Hard Way' is very good and free.";
-	structuredPHP= "There is a great Udemy course titled 'How to become a web developer from scratch!'";
+	structuredPython = "Zed Shaw's 'Learn Python the Hard Way' is very good and free.";
+	structuredPHP = "There is a great Udemy course titled 'How to become a web developer from scratch!'";
 	
-	independentPHP= "There is a great Udemy course titled 'How to become a web developer from scratch!'";
-	independentPython= "Zed Shaw's 'Learn Python the Hard Way' is very good and free.";
+	independentPHP = "There is a great Udemy course titled 'How to become a web developer from scratch!'";
+	independentPython = "Zed Shaw's 'Learn Python the Hard Way' is very good and free.";
 	independentRuby = "Michael Hartl's Ruby Tutorial is often recommended."
 	
-	if (learningStyleSelection == "Independent" && backendSelection == "PHP"){
+	if (learningStyleSelection === "Independent" && backendSelection == "PHP"){
 		document.getElementById('conclusionDummy').innerHTML=independentPHP;
 	}
-		
-	else if (learningStyleSelection == "Independent"  && backendSelection == "Python"){
+	else if (learningStyleSelection === "Independent"  && backendSelection == "Python"){
 		document.getElementById('conclusionDummy').innerHTML=independentPython;
 	}
-	else if (learningStyleSelection == "Structured"  && backendSelection == "Ruby"){
+	else if (learningStyleSelection === "Structured"  && backendSelection == "Ruby"){
 		document.getElementById('conclusionDummy').innerHTML=structuredRuby;
 	}
-	else if (learningStyleSelection == "Independent" && backendSelection == "Ruby"){
+	else if (learningStyleSelection === "Independent" && backendSelection == "Ruby"){
 		document.getElementById('conclusionDummy').innerHTML = independentRuby;
 	}
 };
 
 function oneMoreThing(){
 	oneMoreThingMessage = "No matter what particular path you choose...build, build, build! Why do you think I made this stupid page?! To see more of my creations or learn more about me";
-	document.getElementById('oneMoreThingDummy').innerHTML= oneMoreThingMessage;
+	document.getElementById('oneMoreThingDummy').innerHTML = oneMoreThingMessage;
 };
